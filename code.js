6e894064a76d7a03babe255bc37fe341b324ced7
detectiveAvery <izazhaidermail@gmail.com>
2025-09-22 12:02:32 +0500

    feat: Add logic for random computer choice

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+let computerChoice;
+
+function getComputerChoice(computerChoice) {
+  const randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    return computerChoice = "rock";
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    return computerChoice = "paper";
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    return computerChoice = "scissors";
+  }
+}
+
+console.log(getComputerChoice(computerChoice));
